c5234b70ca666e6fbbeb1cf46ef37c56eb0c48c6
kinMuni <kiny.muni@gmail.com>
2018-10-03 10:48:52 -0400

    -a

--- src/Component/ShelfUpdater.js
+++ src/Component/ShelfUpdater.js
@@ -1,58 +1,64 @@
-import React, { Component } from "react";
+import React, { Component} from "react";
 import PropTypes from "prop-types";
 
 
+
 class ShelfUpdater extends Component {
-	static PropTypes = {
-		book: PropTypes.object.isRequired,
-		updateShelf: PropTypes.func.isRequired
+    static PropTypes = {
+        book: PropTypes.object.isRequired,
+        updateShelf: PropTypes.func.isRequired
 
-	};
-     
-     state= {
+    };
 
-     	currentShelf: this.props.book.shelf,
-     	updating: false
-     };
-         // update shelf method
+    state = {
 
-     updateShelf= (evt) => {
-     	this.props.updateShelf(this.props.book, evt.target.value);
-     	this.setState({
-     		currentShelf:evt.target.value,
-     		updating: true
-     	});
-
-     };
-  ComponentWillReceiveProps() {
-  	// 
-  	this.setState ({
-
-  		updating: false
-  	});
-  }
-  
-  render () {
-
-  	return (
-
-  		<div className="book-shelf-changer">
-                <select
-                    value={this.state.currentShelf}
-                    onChange={this.updateShelf}
-                >
-                    <option value="move" disabled>Move to...</option>
-                    <option value="currentlyReading">Currently Reading</option>
-                    <option value="wantToRead">Want to Read</option>
-                    <option value="read">Read</option>
-                    <option value="none">None</option>
-                </select>
-                { this.state.updating && (<div className="cssload-spin-box"></div>)}
-            </div>
-
-  		)
-  }
+        currentShelf: this.props.book.shelf,
+        updating: false
+    };
 
 
-}
-export default ShelfUpdater;
+    updateShelf = (event) => {
+        this.props.updateShelf(this.props.book, event.target.value);
+        this.setState({
+            currentShelf: event.target.value,
+            updating: true
+        });
+
+    };
+    ComponentWillReceiveProps() {
+
+        this.setState({
+
+            updating: false
+        });
+    }
+
+    render() {
+
+            return (
+
+                <
+                div className = "book-shelf-changer" >
+                <
+                select value = {
+                    this.state.currentShelf
+                }
+                onChange = {
+                    this.updateShelf
+                } >
+                <
+                option value = "move"
+                disabled > Move to... < /option> <
+                option value = "currentlyReading" > Currently Reading < /option> <
+                option value = "wantToRead" > Want to Read < /option> <
+                option value = "read" > Read < /option> <
+                option value = "none" > None < /option> < /
+                select > {
+                    this.state.updating && ( < div className = "cssload-spin-box" > < /div>)} < /
+                        div >
+                    )
+                }
+
+            }
+
+            export default ShelfUpdater
